962d25ec8e03853acf1ca225bde155517650bc87
Mout Pessemier <moutpessemier@hotmail.com>
2019-10-06 17:22:53 +0200

    V1.0.0 - project initialization

---
+++ Flight 'n Bite/Flight 'n Bite/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Flight__n_Bite.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Flight__n_Bite.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ Flight 'n Bite/Flight 'n Bite/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Flight__n_Bite.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Flight__n_Bite.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
