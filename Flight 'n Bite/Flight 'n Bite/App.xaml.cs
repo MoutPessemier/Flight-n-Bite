1c3623b50f7ed6b5394fa1bb81444d2dd7f78533
Mout Pessemier <moutpessemier@hotmail.com>
2019-10-15 19:09:27 +0200

    [FEAT] - added login screen

--- Flight 'n Bite/Flight 'n Bite/App.xaml.cs
+++ Flight 'n Bite/Flight 'n Bite/App.xaml.cs
@@ -47,7 +47,7 @@ namespace Flight__n_Bite
         public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
             // TODO: add your long-running task here
-            await NavigationService.NavigateAsync(typeof(Views.MainPage));
+            await NavigationService.NavigateAsync(typeof(Views.Login));
         }
     }
 }
