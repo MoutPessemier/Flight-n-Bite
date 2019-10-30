6a18fad944ce88ea71f9db6451addcf0d54af350
Jef Malfliet <jef.malfliet@student.hogent.be>
2019-10-30 12:43:45 +0100

    [FEAT] -aanpassen httpservice - foodsendrinks

--- Flight 'n Bite/Flight 'n Bite/ViewModels/FoodsDrinksPageViewModel.cs
+++ Flight 'n Bite/Flight 'n Bite/ViewModels/FoodsDrinksPageViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Flight__n_Bite.Models;
+﻿using Flight__n_Bite.data;
+using Flight__n_Bite.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,21 @@ namespace Flight__n_Bite.ViewModels
     public class FoodsDrinksPageViewModel : ViewModelBase
     {
         private readonly List<Order> _ordersFromPassenger;
-        public List<Product> products 
+        public ObservableCollection<Product> Products { get; set; }
+        private HttpService _httpService;
         public FoodsDrinksPageViewModel(List<Order> orders)
         {
             _ordersFromPassenger = orders;
+            _httpService = HttpService.instance;
+            Products = _httpService.GetProducts();
         }
 
+        public FoodsDrinksPageViewModel()
+        {
+            _ordersFromPassenger = new List<Order>();
+            _httpService = HttpService.instance;
+            Products = _httpService.GetProducts();
+        }
 
     }
 }
